bf0af75b63d92350909be4325ba4988efefdf3a9
Alejandro Matamala <matamala@material.cl>
2017-02-03 15:26:48 -0500

    No-Message

--- zine.js
+++ zine.js
@@ -5,6 +5,8 @@ var exec = require('child_process').exec;
 var clc = require('cli-color');
 var fs = require('fs')
 var createHTML = require('create-html')
+var glob = require("glob")
+var imgs;
 
 // console.log(clc.green(process.argv[1]));
 
@@ -14,23 +16,46 @@ var arg1 = process.argv[2];
 // If Command is build then:
 
 if (arg1 == 'build') {
-  console.log(clc.red('I just created an HTML √√ '));
+  // fs.readdir('./', function(err, files) {
+  //
+  // if (err) console.log(err);
+  //
+  // console.log(files);
+  //
+  // for (var i = 0; i < files.length; i++) {
+  //
+  // }
 
-  var html = createHTML({
-    title: 'example',
-    script: 'example.js',
-    css: 'example.css',
-    lang: 'en',
-    head: '<meta name="description" content="example">',
-    body: '<h1>Your images</h1><br><img src="download.jpg">',
-    favicon: 'favicon.png'
+
+glob("**/*.jpg", function (er, files) {
+
+    // console.log(files);
+
+    for(var i = 0; i < files.length; i++) {
+       imgs = '<img src=' + files[i] + '>'; // Todo agregar comillas en files[i]
+    }
+    console.log(imgs);
+
+
+    var html = createHTML({
+      title: 'Your images',
+      script: 'example.js',
+      css: 'example.css',
+      lang: 'en',
+      head: '<meta name="description" content="example">',
+      body: '<h1>Your images</h1> <br>' + imgs,
+      favicon: 'favicon.png'
+    })
+
+  // Output the Index.html
+  fs.writeFile('index.html', html, function (err) {
+    if (err) console.log(err)
+
+    console.log(clc.red('I just created an HTML √√ '));
+
   })
 
-// Output the Index.html
-fs.writeFile('index.html', html, function (err) {
-  if (err) console.log(err)
 })
-
 } else {
   console.log( "it is not a command");
 }
