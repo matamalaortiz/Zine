6417c17979558151e9e580fea9337473c870ac2a
Alejandro Matamala <matamala@material.cl>
2017-02-05 20:07:09 -0500

    No-Message

--- load.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-var exec = require('child_process').exec;
-var clc = require('cli-color');
-
-console.log(clc.green("second"));
--- zine.js
+++ zine.js
@@ -3,7 +3,8 @@
 // Modules
 var exec = require('child_process').exec;
 var clc = require('cli-color');
-var fs = require('fs')
+var fs = require('fs');
+var path = require('path');
 var createHTML = require('create-html')
 var glob = require("glob") // Filter
 var imgs = [];
@@ -17,35 +18,42 @@ var arg1 = process.argv[2];
 
 if (arg1 == 'build') {
 
-glob("**/*.jpg", function (er, files) {
+    glob("**/*.jpg", function(er, files) {
+
+        for (var i = 0; i < files.length; i++) {
+            imgs[i] = '<img src=' + '"' + files[i] + '"' + '>';
+        }
+        // console.log(imgs);
 
 
-    for (var i = 0; i < files.length; i++) {
-       imgs[i] = '<img src=' + '"'  + files[i] + '"' + '>'; // Todo agregar comillas en files[i]
-    }
-    // console.log(imgs);
+        var html = createHTML({
+            title: 'A Simple Website with your images',
+            script: 'example.js',
+            css: 'mystyle.css',
+            lang: 'en',
+            favicon: 'favicon.png',
+            head: '<meta name="description" content="example">',
+            body: '<h1 style=' + '"' + 'position: fixed;' + 'margin-left: 40%;' + 'font-family: helvetica;' + '"' + '> A Simple Website</h1> <br>' + imgs
+        })
 
+        // Output the Index.html
+        fs.writeFile('index.html', html, function(err) {
+            if (err) console.log(err)
 
-    var html = createHTML({
-      title: 'Your images',
-      script: 'example.js',
-      css: 'example.css',
-      lang: 'en',
-      head: '<meta name="description" content="example">',
-      body: '<h1>A Simple Website with your images:</h1> <br>' + imgs,
-      favicon: 'favicon.png'
+            console.log(clc.green("Congrats!! You've just created a website with your images ! "));
+
+        })
+
+        var style = ' "body { background-color: white;} img { width: auto; max-width: 40vw; max-height: auto; padding: 9px; margin-top: 5%; margin-left: 5%;} @media screen and (max-device-width: 767px){ img { width: 100%; max-width: 100vw; max-height: auto; padding: 56px; margin-top: 10%; margin-left: 0%; margin-bottom:-14%; }} h1 { text-decoration: underline;}" '
+        var createStyle = 'echo' + style + ' >> mystyle.css';
+        exec(createStyle, {}, function() {
+            // console.log(createStyle);
+        });
+
+        
+
     })
 
-  // Output the Index.html
-  fs.writeFile('index.html', html, function (err) {
-    if (err) console.log(err)
-
-    console.log(clc.green('Wohooo ! You have just created a website with your images ! '));
-
-  })
-
-})
-
 } else {
-  console.log( "it is not a command");
+    console.log("it is not a command");
 }
